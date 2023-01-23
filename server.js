faa107c48f803e899b18f22d7d0e9068939b7309
Devin Kearney <devinkearney@gmail.com>
2023-01-22 21:39:15 -0400

    Built the routes to get and post

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 //Route for API
+//Route for getting
+app.get("/api/notes", (req, res) => {
+    //Retrieves the note list from the db
+    const noteList = JSON.parse(fs.readFileSync("./db/db.json"))
+    //Responds with the note list
+    res.json(noteList);
+});
+//Route for posting
+app.post("/api/notes", (req, res) => {
+    //Creates a new note using the body of the request
+    const noteNew = req.body;
+    //Retrieves the note list from the db
+    const noteList = JSON.parse(fs.readFileSync("./db/db.json"))
+    //Gives each new note an ID = to its place on the list
+    noteNew.id = noteList.length + 1;
+    //Adds the new note to the end of the note list
+    noteList.push(noteNew);
+    //Writes the new note list back in the db
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList))
+    //Responds with the note list
+    res.json(noteList);
+});
