64c35eced30e550a65d277ff1ef4326574bb2274
Devin Kearney <devinkearney@gmail.com>
2023-01-22 21:46:11 -0400

    Created route for deleting notes, added a variable to speed up coding

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+//A variable to speed up coding
+const database = "./db/db.json";
+
 const app = express();
 //Set up port for app
 const PORT = process.env.PORT || 3033;
@@ -18,7 +21,7 @@ app.use(express.json());
 //Route for getting
 app.get("/api/notes", (req, res) => {
     //Retrieves the note list from the db
-    const noteList = JSON.parse(fs.readFileSync("./db/db.json"))
+    const noteList = JSON.parse(fs.readFileSync(database))
     //Responds with the note list
     res.json(noteList);
 });
@@ -27,13 +30,21 @@ app.post("/api/notes", (req, res) => {
     //Creates a new note using the body of the request
     const noteNew = req.body;
     //Retrieves the note list from the db
-    const noteList = JSON.parse(fs.readFileSync("./db/db.json"))
+    const noteList = JSON.parse(fs.readFileSync(database))
     //Gives each new note an ID = to its place on the list
     noteNew.id = noteList.length + 1;
     //Adds the new note to the end of the note list
     noteList.push(noteNew);
     //Writes the new note list back in the db
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList))
+    fs.writeFileSync(database, JSON.stringify(noteList))
     //Responds with the note list
     res.json(noteList);
+});
+
+//Route for deleting
+app.delete("/api/notes/:id", (req, res) => {
+    const noteList = JSON.parse(fs.readFileSync(database))
+    const noteDelete = noteList.filter((deleteNote) => deleteNote.id !== req.params.id);
+    fs.writeFileSync(database, JSON.stringify(noteDelete));
+    res.json(noteDelete);
 });
