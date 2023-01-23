fd3f0ba6c98aa45346a491b7a258803e9c3a9243
Devin Kearney <devinkearney@gmail.com>
2023-01-22 21:30:06 -0400

    set up port and middleware

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+//Packages needed for server.js
+const exp = require("constants");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+
+const app = express();
+//Set up port for app
+const PORT = process.env.PORT || 3033;
+
+//Code for middleware
+//Use the ./public directory
+app.use(express.static("./public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Route for API
