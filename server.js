9f0f4f2c8c8271159fbea489b1b54488d8556eb0
Devin Kearney <devinkearney@gmail.com>
2023-01-23 12:39:47 -0400

    Found a removed a bug making application not switch to /notes

--- server.js
+++ server.js
@@ -1,67 +1,63 @@
 //Packages needed for server.js
-const exp = require("constants");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-//A variable to speed up coding
-const database = "./db/db.json";
-
+const db = "./db/db.json";
 const app = express();
 //Set up port for app
-const PORT = process.env.PORT || 3033;
+const PORT = process.env.PORT || 3001;
 
 //Code for middleware
 //Use the ./public directory
-app.use(express.static("./public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("./public"));
 
 //Function for getting
 app.get("/api/notes", (req, res) => {
     //Retrieves the note list from the db
-    const noteList = JSON.parse(fs.readFileSync(database))
+    const noteList = JSON.parse(fs.readFileSync(db))
     //Responds with the note list
     res.json(noteList);
 });
+
 //Function for posting
 app.post("/api/notes", (req, res) => {
     //Creates a new note using the body of the request
     const noteNew = req.body;
     //Retrieves the note list from the db
-    const noteList = JSON.parse(fs.readFileSync(database))
+    const noteList = JSON.parse(fs.readFileSync(db))
     //Gives each new note an ID = to its place on the list
     noteNew.id = noteList.length + 1;
     //Adds the new note to the end of the note list
     noteList.push(noteNew);
     //Writes the new note list back in the db
-    fs.writeFileSync(database, JSON.stringify(noteList))
+    fs.writeFileSync(db, JSON.stringify(noteList))
     //Responds with the note list
     res.json(noteList);
 });
 
-//Function for deleting
 app.delete("/api/notes/:id", (req, res) => {
-    const noteList = JSON.parse(fs.readFileSync(database))
+    const noteList = JSON.parse(fs.readFileSync(db))
     const noteDelete = noteList.filter((deleteNote) => deleteNote.id !== req.params.id);
-    fs.writeFileSync(database, JSON.stringify(noteDelete));
+    fs.writeFileSync(db, JSON.stringify(noteDelete));
     res.json(noteDelete);
 });
-
 //Calls to the HTML
 //Wildcard, if anything is put in address, go to index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 //If nothing is put in address after /, go to index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 //If notes is placed at the ned of address, go to notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listening at http://localhost:${PORT}`);
+    console.log(`Application is listening at http://localhost:${PORT}`);
 });
